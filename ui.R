bfb3f71a12d63a96ef494b6d0c2b067ba9bf390e
kikihatzistavrou <kikihatzistavrou@gmail.com>
2015-05-11 12:26:15 +0300

    no message

--- ui.R
+++ ui.R
@@ -1,3 +1,33 @@
+# Course Project for Coursera Course : "Developing Data Products" 
+# 
+# This is the course project for "Developing Data Products" Course of Johns 
+# Hopkins University (Coursera).
+# It is a web app implemented with R and shiny package.
+# 
+# It implements a BMI(Bodily Mass Index) Calculator.
+# 
+# To run the apllication you must have:
+# 1) Installed R and Rstidio
+# 2) Create a folder and put server.R ui.R and folder images in it.
+# 3) In R Studio : 
+#    a) setwd("folder_path") (eg. setwd("C:/Users/Kiki/Documents/bmi") if 
+#       the name of the folder that you created is "bmi")
+#     b) install.packages("shiny")
+#     c) library(shiny)
+#     d) runApp()
+# 4) And done!
+#       
+# To use the application:
+# 1) Select your Gender. Click on the radio button to choose.
+# 2) Move the slider in order to define your weight.
+# 3) Move the slider in order to define your height.
+# 4) In the main panel you can see:
+#       a) the weight you entered
+#       b) the height ypu entered
+#       c) your bmi
+#       d) the meaning of your bmi and some advice about you own health.
+
+
 library(shiny)
 shinyUI(pageWithSidebar(
       headerPanel("Bodily Mass Index (BMI) Calculator"),
